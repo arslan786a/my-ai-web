aa0e3e7ac81397a84bda3b918b68a5965c3c4543
arslan786a <nadeemkashifa.a@gmail.com>
2025-09-10 13:23:01 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-// callMain now receives log function from index.html
+// This function will be called from index.html
 window.callMain = function(logFn) {
   logFn("main.jsx picked the call.");
 
   logFn("Calling App.jsx...");
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<App logFn={logFn}/>);
+  root.render(<App logFn={logFn} />);
 
   logFn("main.jsx finished calling App.jsx.");
 };
